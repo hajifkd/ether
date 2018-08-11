39e5e30a97529c6cd2351e1d9aa32abcd07cc472
Hajime Fukuda <hajime.fukuda@me.com>
2018-08-12 00:34:57 +0900

    modified the sample code of hyper

--- src/bin/hyper_sample.rs
+++ src/bin/hyper_sample.rs
@@ -1,15 +1,14 @@
-extern crate hyper;
 extern crate futures;
+extern crate hyper;
 
 use futures::{future, Future};
-use hyper::{Server, Response, Body, Method, StatusCode, Request};
 use hyper::service::service_fn;
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
 
 const INDEX: &[u8] = b"hogehoge";
 const NOT_FOUND: &[u8] = b"Not Found";
 
-fn response(req: Request<Body>) -> Box<Future<Item=Response<Body>, Error=hyper::Error> + Send>
-{
+fn response(req: Request<Body>) -> Box<Future<Item = Response<Body>, Error = hyper::Error> + Send> {
     println!("Request received: {}", req.uri());
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/") => {
@@ -19,27 +18,24 @@ fn response(req: Request<Body>) -> Box<Future<Item=Response<Body>, Error=hyper::
 
         _ => {
             let body = Body::from(NOT_FOUND);
-            Box::new(future::ok(Response::builder()
-                                         .status(StatusCode::NOT_FOUND)
-                                         .body(body)
-                                         .unwrap()))
+            Box::new(future::ok(
+                Response::builder()
+                    .status(StatusCode::NOT_FOUND)
+                    .body(body)
+                    .unwrap(),
+            ))
         }
     }
 }
 
-
 fn main() {
     let addr = "127.0.0.1:8080".parse().unwrap();
     hyper::rt::run(future::lazy(move || {
-        let new_service = || {
-            service_fn(|req| {
-                response(req)
-            })
-        };
+        let new_service = || service_fn(|req| response(req));
 
         let server = Server::bind(&addr)
-                            .serve(new_service)
-                            .map_err(|e| eprintln!("Error: {}", e));
+            .serve(new_service)
+            .map_err(|e| eprintln!("Error: {}", e));
 
         println!("Listening on {}", addr);
 
